6f403ff7797ce10330ef1db41373524cb341580a
Florian Moesch <florian@moesch.org>
2024-10-26 20:24:17 +0200

    move mail via RT to plugin

--- pretalx_rt/mail.py
+++ pretalx_rt/mail.py
@@ -10,20 +10,31 @@ class RTBackend(BaseEmailBackend):
     def send_messages(self, email_messages):
         for message in email_messages:
             event = Event.objects.get(pk=message.extra_headers["X-Pretalx-Event"])
-            if "X-Pretalx-RT-Ticket" not in message.extra_headers:
-                """38C3 hack in lack of a plugin"""
-                new_ticket = {
-                    "Requestor": message.to,
-                    "Status": "resolved",
-                    "Owner": "Nobody",
-                }
+            ticket_id = None
+            if "X-Pretalx-RT-Ticket" in message.extra_headers:
+                ticket_id = message.extra_headers["X-Pretalx-RT-Ticket"]
+            else:
                 c = rt.rest2.Rt(
                     url=event.settings.rt_rest_api_url,
                     token=event.settings.rt_rest_api_key,
                 )
-                self.ticket_id = c.create_ticket(
+                ticket_id = c.create_ticket(
                     event.settings.rt_queue,
                     subject=message.subject,
                     content="Ticket automatically created by pretalx",
-                    **new_ticket,
+                    Requestor=message.to,
+                    Status="resolved",
+                    Owner="Nobody",
+                )
+
+            ticket = c.get_ticket(ticket_id)
+            try:
+                c.edit_ticket(ticket_id, Requestor=message.to, Subject=message.subject)
+                c.reply(ticket_id, content=message.body, content_type="text/plain")
+            finally:
+                c.edit_ticket(
+                    ticket_id,
+                    Requestor=ticket["Requestor"],
+                    Subject=ticket["Subject"],
+                    Status=ticket["Status"],
                 )
