b38f9dabd55e2b06fc7a4c9d4cc8b4cc4cfed5bd
Florian Moesch <florian@moesch.org>
2024-12-09 17:25:32 +0100

    rework signals

--- pretalx_rt/ui_signals.py
+++ pretalx_rt/ui_signals.py
@@ -2,13 +2,11 @@ import logging
 
 from django.dispatch import receiver
 from django.urls import reverse
+
 from pretalx.common.signals import register_data_exporters
-from pretalx.mail.signals import html_after_mail_badge, html_below_mail_subject
+from pretalx.mail.signals import mail_form_html
 from pretalx.orga.signals import nav_event_settings
-from pretalx.submission.signals import (
-    html_below_submission_form,
-    html_below_submission_link,
-)
+from pretalx.submission.signals import submission_form_html, submission_form_link
 
 logger = logging.getLogger(__name__)
 
@@ -37,7 +35,7 @@ def pretalx_rt_data_exporter(sender, **kwargs):
     return Exporter
 
 
-@receiver(html_after_mail_badge)
+# @receiver(html_after_mail_badge)
 def pretalx_rt_html_after_mail_badge(sender, request, mail, **kwargs):
     result = ""
     for ticket in mail.rt_tickets.all():
@@ -46,22 +44,12 @@ def pretalx_rt_html_after_mail_badge(sender, request, mail, **kwargs):
     return result
 
 
-@receiver(html_below_mail_subject)
-def pretalx_rt_html_below_mail_subject(sender, request, mail, **kwargs):
+@receiver(mail_form_html)
+def pretalx_rt_mail_form_html(sender, request, mail, **kwargs):
     if not request.user.has_perm("orga.view_mails", request.event):
         return None
     result = ""
     for ticket in mail.rt_tickets.all():
-        result += '<i class="fa fa-check-square-o" title="Request Tracker"></i> '
-        result += f'<a href="{sender.settings.rt_url}Ticket/Display.html?id={ticket.id}">{ticket.id}</a>: '
-        result += f"<small>{ticket.subject} ({ticket.status} in queue {ticket.queue})</small> "
-    return result
-
-
-@receiver(html_below_submission_form)
-def pretalx_rt_html_below_submission_form(sender, request, submission, **kwargs):
-    result = ""
-    if hasattr(submission, "rt_ticket"):
         result += '<div class="form-group row">'
         result += '<label class="col-md-3 col-form-label">'
         result += "Request Tracker"
@@ -69,17 +57,38 @@ def pretalx_rt_html_below_submission_form(sender, request, submission, **kwargs)
         result += '<div class="col-md-9">'
         result += '<div class="pt-2">'
         result += '<i class="fa fa-check-square-o"></i> '
-        result += f'<a href="{sender.settings.rt_url}Ticket/Display.html?id={submission.rt_ticket.id}">{submission.rt_ticket.id}</a> : '
-        result += f"{submission.rt_ticket.subject}"
-        result += f'<small class="form-text text-muted">{submission.rt_ticket.status} in queue {submission.rt_ticket.queue}</small>'
+        result += f'<a href="{sender.settings.rt_url}Ticket/Display.html?id={ticket.id}">{ticket.id}</a> : '
+        result += f"{ticket.subject}"
+        result += f'<small class="form-text text-muted">{ticket.status} in queue {ticket.queue}</small>'
         result += "</div>"
         result += "</div>"
         result += "</div>"
     return result
 
 
-@receiver(html_below_submission_link)
-def pretalx_rt_html_below_submission_link(sender, request, submission, **kwargs):
+@receiver(submission_form_html)
+def pretalx_rt_submission_form_html(sender, request, submission, **kwargs):
+    result = ""
+    if hasattr(submission, "rt_ticket"):
+        ticket = submission.rt_ticket
+        result += '<div class="form-group row">'
+        result += '<label class="col-md-3 col-form-label">'
+        result += "Request Tracker"
+        result += "</label>"
+        result += '<div class="col-md-9">'
+        result += '<div class="pt-2">'
+        result += '<i class="fa fa-check-square-o"></i> '
+        result += f'<a href="{sender.settings.rt_url}Ticket/Display.html?id={ticket.id}">{ticket.id}</a> : '
+        result += f"{ticket.subject}"
+        result += f'<small class="form-text text-muted">{ticket.status} in queue {ticket.queue}</small>'
+        result += "</div>"
+        result += "</div>"
+        result += "</div>"
+    return result
+
+
+@receiver(submission_form_link)
+def pretalx_rt_submission_form_link(sender, request, submission, **kwargs):
     result = ""
     if hasattr(submission, "rt_ticket"):
         result += f'<a href="{sender.settings.rt_url}Ticket/Display.html?id={submission.rt_ticket.id}" class="dropdown-item" role="menuitem" tabindex="-1">'
