6b2416517a54b9437c119038615764ff0bf1d1ad
Florian Moesch <florian@moesch.org>
2025-08-18 13:48:41 +0200

    rework signals

--- pretalx_rt/signals.py
+++ pretalx_rt/signals.py
@@ -3,17 +3,84 @@ from datetime import timedelta
 
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
+from django.template import loader
+from django.urls import reverse
 from django.utils.timezone import now
-from pretalx.common.signals import minimum_interval, periodic_task
+from pretalx.common.signals import (
+    minimum_interval,
+    periodic_task,
+    register_data_exporters,
+)
 from pretalx.mail.signals import queuedmail_pre_send
+from pretalx.orga.signals import mail_form, nav_event_settings, submission_form
 from pretalx.person.models import User
 from pretalx.submission.models import Submission
 from pretalx.submission.signals import submission_state_change
 from rt.rest2 import Attachment, Rt
 
+from .forms import RTForm
 from .models import Ticket
 
 logger = logging.getLogger(__name__)
+
+
+@receiver(nav_event_settings)
+def pretalx_rt_settings(sender, request, **kwargs):
+    if not request.user.has_perm("event.update_event", request.event):
+        return []
+    return [
+        {
+            "label": "RT",
+            "url": reverse(
+                "plugins:pretalx_rt:settings",
+                kwargs={"event": request.event.slug},
+            ),
+            "active": request.resolver_match.url_name == "plugins:pretalx_rt:settings",
+        }
+    ]
+
+
+@receiver(register_data_exporters, dispatch_uid="exporter_rt")
+def pretalx_rt_data_exporter(sender, **kwargs):
+    logger.info("exporter registration")
+    from .exporter import Exporter
+
+    return Exporter
+
+
+@receiver(mail_form)
+def pretalx_rt_mail_form(sender, request, instance, **kwargs):
+    forms = []
+    for ticket in instance.rt_tickets.all():
+        forms.append(RTForm(instance=ticket, event=sender))
+    return forms
+
+
+@receiver(submission_form)
+def pretalx_rt_submission_form(sender, request, instance, **kwargs):
+    forms = []
+    if hasattr(instance, "rt_ticket"):
+        forms.append(RTForm(instance=instance.rt_ticket, event=sender))
+    return forms
+
+
+try:
+    from samaware.signals import submission_html
+
+    @receiver(submission_html)
+    def samaware_submission_html(sender, request, submission, **kwargs):
+        if hasattr(submission, "rt_ticket"):
+            tickets = [submission.rt_ticket]
+            template = loader.get_template("pretalx_rt/samaware.html")
+            context = {
+                "event": sender,
+                "tickets": tickets,
+            }
+            return template.render(context, None)
+        return None
+
+except ImportError:
+    pass
 
 
 @receiver(periodic_task)
@@ -197,6 +264,3 @@ def pretalx_rt_pull(event, ticket):
 
 def requestors(users):
     return [f"{user.name.replace('@', '(at)')} <{user.email}>" for user in users]
-    # return ",".join(
-    #     f"{user.name.replace('@', '(at)')} <{user.email}>" for user in users
-    # )
