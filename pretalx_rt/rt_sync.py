4c438bdb015dccf058176d5423c937800ad88bb6
Florian Moesch <florian@moesch.org>
2025-08-21 09:33:46 +0200

    synchronize whole queue at once

--- pretalx_rt/rt_sync.py
+++ pretalx_rt/rt_sync.py
@@ -12,7 +12,7 @@ class RTSync:
     def __init__(self, event, user=None):
         self.event = event
         self.logger = logging.getLogger(__name__)
-        if user and user.rt_settings.rest_auth_token:
+        if user and hasattr(user, "rt_settings") and user.rt_settings.rest_auth_token:
             token = user.rt_settings.rest_auth_token
         else:
             token = event.rt_settings.rest_auth_token
@@ -25,7 +25,7 @@ class RTSync:
         """Create a new RT ticket for a submission."""
         self.logger.info(f"Creating RT ticket for submission {submission.code}")
 
-        id = self.rt.create_ticket(
+        rt_id = self.rt.create_ticket(
             queue=self.event.rt_settings.queue,
             subject=submission.title,
             Requestor=self.requestors(submission.speakers.all()),
@@ -37,7 +37,7 @@ class RTSync:
             },
         )
 
-        ticket = Ticket(event=self.event, rt_id=id, submission=submission)
+        ticket = Ticket(event=self.event, rt_id=rt_id, submission=submission)
         self.pull(ticket)
         return ticket
 
@@ -45,7 +45,7 @@ class RTSync:
         """Create a new RT ticket for a standalone mail."""
         self.logger.info("Creating RT ticket for standalone mail")
 
-        id = self.rt.create_ticket(
+        rt_id = self.rt.create_ticket(
             queue=self.event.rt_settings.queue,
             subject=mail.subject,
             Requestor=self.requestors(mail.to_users.all()),
@@ -53,13 +53,13 @@ class RTSync:
             Owner="Nobody",
         )
 
-        ticket = Ticket(event=self.event, rt_id=id)
+        ticket = Ticket(event=self.event, rt_id=rt_id)
         self.pull(ticket)
         return ticket
 
     def add_mail_to_ticket(self, ticket, mail):
         """Add a mail as a reply to an RT ticket."""
-        self.logger.info(f"Adding mail to RT ticket {ticket.rt_id}")
+        self.logger.info(f"Adding mail to RT #{ticket.rt_id}")
         old_ticket = self.rt.get_ticket(ticket.rt_id)
 
         try:
@@ -101,7 +101,7 @@ class RTSync:
 
     def add_comment_to_ticket(self, ticket, comment):
         """Add a comment to an RT ticket."""
-        self.logger.info(f"Adding comment to RT ticket {ticket.rt_id}")
+        self.logger.info(f"Adding comment to RT #{ticket.rt_id}")
         old_ticket = self.rt.get_ticket(ticket.rt_id)
         from pretalx.common.templatetags.rich_text import rich_text
 
@@ -124,7 +124,7 @@ class RTSync:
         if not ticket.submission:
             return
 
-        self.logger.info(f"Pushing updates to RT ticket {ticket.rt_id}")
+        self.logger.info(f"Pushing updates to RT #{ticket.rt_id}")
 
         self.rt.edit_ticket(
             ticket.rt_id,
@@ -141,7 +141,7 @@ class RTSync:
 
     def pull(self, ticket):
         """Pull updates from RT ticket to pretalx."""
-        self.logger.info(f"Pulling updates from RT ticket {ticket.rt_id}")
+        self.logger.info(f"Pulling updates from RT #{ticket.rt_id}")
         rt_ticket = self.rt.get_ticket(ticket.rt_id)
 
         ticket.subject = rt_ticket["Subject"]
@@ -154,6 +154,93 @@ class RTSync:
     def requestors(self, users):
         """Format user information for RT requestors field."""
         return [f"{user.name.replace('@', '(at)')} <{user.email}>" for user in users]
+
+    @staticmethod
+    def get_custom_field(rt_ticket, field_name):
+        """Extract a custom field value from an RT ticket dict."""
+        for field in rt_ticket.get("CustomFields", []):
+            if field.get("name") == field_name:
+                values = field.get("values")
+                if values:
+                    return values[0]
+        return None
+
+    def sync_queue(self):
+        """Sync RT queue to Pretalx tickets, creating submission links if valid."""
+        queue_name = self.event.rt_settings.queue
+        self.logger.info(f"Starting sync of RT queue '{queue_name}'")
+
+        submissions_by_code = {
+            s.code: s for s in self.event.submissions.prefetch_related("speakers").all()
+        }
+
+        existing_tickets = {
+            t.rt_id: t
+            for t in Ticket.objects.filter(event=self.event).select_related(
+                "submission"
+            )
+        }
+
+        tickets = list(self.rt.search(queue_name))
+        total = len(tickets)
+        new_tickets = 0
+        updated_tickets = 0
+
+        for rt_ticket in tickets:
+            pretalx_id = self.get_custom_field(
+                rt_ticket, self.event.rt_settings.custom_field_id
+            )
+            if not pretalx_id:
+                self.logger.debug(f"Skipping RT #{rt_ticket['id']} (no Submission)")
+                continue
+
+            submission = submissions_by_code.get(pretalx_id)
+            if not submission:
+                self.logger.warning(
+                    f"Skipping RT #{rt_ticket['id']}: Submission '{pretalx_id}' not found"
+                )
+                continue
+
+            ticket = Ticket.objects.filter(
+                event=self.event, submission=submission
+            ).first()
+            if ticket:
+                if ticket.rt_id == int(rt_ticket["id"]):
+                    self.logger.debug(
+                        f"Skipping RT #{rt_ticket['id']}: Submission '{pretalx_id}' already linked."
+                    )
+                else:
+                    self.logger.warning(
+                        f"Skipping RT #{rt_ticket['id']}: Submission '{pretalx_id}' already linked to RT #{ticket.rt_id}."
+                    )
+                continue
+
+            ticket = existing_tickets.get(rt_ticket["id"])
+            if ticket:
+                ticket.submission = submission
+                ticket.save()
+                updated_tickets += 1
+                self.logger.info(
+                    f"Linked RT #{rt_ticket['id']} to Submission '{submission.code}'"
+                )
+                self.push(ticket)
+            else:
+                ticket = Ticket(
+                    event=self.event,
+                    rt_id=rt_ticket["id"],
+                    submission=submission,
+                )
+                ticket.save()
+                new_tickets += 1
+                self.logger.info(
+                    f"Created RT #{rt_ticket['id']} for Submission '{submission.code}'"
+                )
+                self.push(ticket)
+
+        self.logger.info(
+            f"Synced {total} tickets of queue '{queue_name}': "
+            f"{updated_tickets} tickets updated, {new_tickets} tickets new."
+        )
 
 
 @app.task(bind=True, name="pretalx_rt.ticket_pull_task")
